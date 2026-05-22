300f039aa88ffd8730d5552c19ec1222456c7f26
Vaishnavi Gaikwad <5076198@mitacsc.edu.in>
2026-05-22 12:16:32 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,7 @@ app.get('/', (req, res) => {
   res.send('Task Manager Backend Running');
 });
 
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
